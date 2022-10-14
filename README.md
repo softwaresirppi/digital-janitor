2976e0e571fdccad09c055656692645240626c64
Wox Tox Fox <dabotguy@gmail.com>
2022-10-14 11:47:08 +0530

    Prompt implemented

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 An automatic file organizer which organizes the files according to the rules set by the user.
-# DONE
+
+# Examples
+```
+Delete all compiled .class files
+/home/crayonie/*.class DO delete(path)
+Move all c files to c_programs directory
+/home/crayonie/*.c DO move(path, '/home/crayonie/c_programs/' + name)
+Prompt and delete the files bigger than 1 GB
+/home/crayonie/* IF size > 1 GB DO delete(path, True)
+Copy the files accessed before half an hour to recents folder
+/home/crayonie/* IF accessed < 30 MIN DO copy(path, '/home/crayonie/recents/' + name)
+Copy the files modified before 12 hours to recents folder
+/home/crayonie/* IF modified < 12 HR DO copy(path, '/home/crayonie/recents/' + name)
+```
+
+
+# Features
  - Predicates
    - path
    - size
@@ -14,6 +30,7 @@ An automatic file organizer which organizes the files according to the rules set
    - Delete
 
 # Feature Roadmap
+ - Add parent predef variable
  - Safe moving
  - Tagging system
  - Prompting
--- janitor.py
+++ janitor.py
@@ -13,12 +13,18 @@ if 'win' in system:
     filedelimiter = "\\"
 else:
     filedelimiter = "/"
-print(filedelimiter)
+
 # glob, glob, glob if pred do action
-def move(f, t):
+def move(f, t, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
     os.renames(f, t)
 
-def copy(f, t):
+def copy(f, t, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
     pathEntities = t.split(filedelimiter)
     parent = filedelimiter.join(pathEntities[0:len(pathEntities) - 1])
     print(parent)
@@ -28,38 +34,57 @@ def copy(f, t):
         pass
     shutil.copy(f, t)
 
-def delete(f):
+def delete(f, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
     os.remove(f)
+
+def prompt(f):
+    print(f + ": Are you sure? (y/n)")
+    reply = input().lower()
+    if reply == 'y':
+        return True
+    else:
+        return False
+
 
 def cmd(line):
     parts = re.split(r'\s+IF|DO\s+', line)
     pattern = parts[0]
-    predicate = parts[1]
+    pattern = pattern.strip()
+    if len(parts) == 2:
+        predicate = 'True'
+    else:
+        predicate = parts[1]
     predicate = predicate.replace('KB', '* 1000')
     predicate = predicate.replace('MB', '* 1000000')
     predicate = predicate.replace('GB', '* 1000000000')
     predicate = predicate.replace('MIN', '* 60')
     predicate = predicate.replace('HR', '* 3600')
     predicate = predicate.replace('DAY', '* 86400')
-    action = parts[2]
+    action = parts[-1]
     global path
     for path in glob.glob(pattern, recursive = True):
-        stat = os.stat(path)
-        global size
-        size = stat.st_size # in bytes
-        global name
-        name = path.split(filedelimiter)[-1]
-        global extension
-        extension = path.split('.')[-1]
-        global mimetype
-        mimetype = mimetypes.guess_type(path)[0]
-        global accessed
-        accessed = time.time() - stat.st_atime
-        global modified
-        modified = time.time() - stat.st_mtime
-        if eval(predicate, globals()) == True:
-            print('PROCESSING ', path)
-            exec(action)
+        if os.path.isfile(path):
+            stat = os.stat(path)
+            global size
+            size = stat.st_size # in bytes
+            global name
+            name = path.split(filedelimiter)[-1]
+            global extension
+            extension = path.split('.')[-1]
+            global mimetype
+            mimetype = mimetypes.guess_type(path)[0]
+            if mimetype == None:
+                mimetype = ''
+            global accessed
+            accessed = time.time() - stat.st_atime
+            global modified
+            modified = time.time() - stat.st_mtime
+            if eval(predicate, globals()) == True:
+                print('PROCESSING ', path)
+                exec(action)
 
 if len(sys.argv) < 2:
     print("No script file is given")
